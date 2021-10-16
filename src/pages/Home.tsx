272bddd58c0b3b20d6bd91df66f04f196133ae53
OXcarXierra <littlestein@snu.ac.kr>
2021-10-16 17:56:27 +0900

    react router dom initial setting

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import Button from '@mui/material/Button';
+import { useHistory } from 'react-router';
+import styled from 'styled-components';
+
+const StyledButton = styled(Button)`
+  width: 100%;
+`;
+
+const Home = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <StyledButton variant="contained" onClick={() => history.push('Login')}>
+        Login
+      </StyledButton>
+      Home
+    </div>
+  );
+};
+
+export default Home;
